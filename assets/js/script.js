664a02e0c0503d6d38ce4c5988fc5cd52971ec7a
BorisMejia <borisfernandez916@gmail.com>
2023-10-12 10:26:29 -0500

    Actualizacion html y css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
-const contenedor = document.querySelector(".container"),
-login = document.querySelector(".container-login header")
-registro = document.querySelector(".container-registro header")
 
-registro.addEventListener('click', ()=>{
+const contenedor = document.querySelector(".container"),
+loginHeader = document.querySelector(".container-login header")
+registroHeader = document.querySelector(".container-registro header")
+
+registroHeader.addEventListener('click', ()=>{
     contenedor.classList.add('active')
 })
-login.addEventListener('click', ()=>{
+loginHeader.addEventListener('click', ()=>{
     contenedor.classList.remove('active')
 })
+
